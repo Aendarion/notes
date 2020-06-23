1300c32b0158349c5c33763e16d24df7ce11f24b
Aendarion <67279960+Aendarion@users.noreply.github.com>
2020-06-23 19:07:57 +0300

    Back-end
    Authentication
    Password reset
    Email validation

---
+++ php/defaultNotes.php
@@ -0,0 +1,12 @@
+<div id="main">
+    <div class="todo" id="newTodo"><input type="text" placeholder="New task" id="todoInput"></div>
+    <div class="todoOptions">
+        <ul class="filters">
+            <li class="selected">All</li>
+            <li>Active</li>
+            <li>Completed</li>
+        </ul>
+        <button class="deleteSelected">Clear completed</button>
+    </div>
+    <button class="save">Save</button>
+</div>
